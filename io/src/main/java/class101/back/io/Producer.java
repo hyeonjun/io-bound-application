72410b98b1951855442738b974f73b452229e3aa
hjju <hjju@fin2b.com>
2023-02-06 00:38:17 +0900

    message send to queue logging

--- io/src/main/java/class101/back/io/Producer.java
+++ io/src/main/java/class101/back/io/Producer.java
@@ -1,17 +1,20 @@
 package class101.back.io;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class Producer {
 
   private final RabbitTemplate rabbitTemplate;
   private static String createPostQueueName = "CREATE_POST_QUEUE";
 
   public void sendTo(String message) {
+    log.info("data send to queue: {}", message);
     this.rabbitTemplate.convertAndSend(createPostQueueName, message);
   }
 
