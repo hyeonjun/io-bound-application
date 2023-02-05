2317518a014862f983b2884b2247387364ad54e3
hjju <hjju@fin2b.com>
2023-02-05 22:08:45 +0900

    해당 내용이 포함된 모든 글 조회

--- io/src/main/java/class101/back/io/PostRepository.java
+++ io/src/main/java/class101/back/io/PostRepository.java
@@ -1,7 +1,9 @@
 package class101.back.io;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+  List<Post> findAllByContentContains(String content);
 }
