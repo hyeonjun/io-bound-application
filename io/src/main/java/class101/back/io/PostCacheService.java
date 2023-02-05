6e910ae32bf8b5aa1d6cb8dfecfed5ba356e87e2
hjju <hjju@fin2b.com>
2023-02-06 01:04:32 +0900

    글 첫 페이지를 캐싱 - 1초마다 새로고침

---
+++ io/src/main/java/class101/back/io/PostCacheService.java
@@ -0,0 +1,27 @@
+package class101.back.io;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PostCacheService {
+
+  private final PostRepository postRepository;
+
+  private Page<Post> firtPostPage;
+
+  @Scheduled(cron = "* * * * * *")
+  public void updateFirstPostPage() {
+    firtPostPage = postRepository.findAll(
+      PageRequest.of(0, 20, Sort.by("id").descending()));
+  }
+
+  public Page<Post> getFirstPostPage() {
+    return this.firtPostPage;
+  }
+}
