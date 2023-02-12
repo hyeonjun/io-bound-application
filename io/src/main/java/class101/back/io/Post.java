1f8e29162ba91a4d71aa658a81a31638e555b93d
hjju <hjju@fin2b.com>
2023-02-12 19:43:50 +0900

    Elasticsearch index에 저장되도록 설정

--- io/src/main/java/class101/back/io/Post.java
+++ io/src/main/java/class101/back/io/Post.java
@@ -9,16 +9,21 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.elasticsearch.annotations.Document;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
-@Entity(name = "post")
+//@Entity(name = "post")
+@Document(indexName = "post")
 public class Post {
+//  @Id
+//  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "post_id_seq")
+//  @SequenceGenerator(name="post_id_seq", sequenceName = "post_id_seq", allocationSize = 1)
+//  private Long id;
+
   @Id
-  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "post_id_seq")
-  @SequenceGenerator(name="post_id_seq", sequenceName = "post_id_seq", allocationSize = 1)
-  private Long id;
+  private String id;
   private String content;
 }
