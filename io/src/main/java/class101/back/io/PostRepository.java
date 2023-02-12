35b9d02902e131e01bc7098d642ea62318c4844d
hjju <hjju@fin2b.com>
2023-02-12 19:44:29 +0900

    JpaRepository 제거ㅓ,ElasticsearchRepository extends

--- io/src/main/java/class101/back/io/PostRepository.java
+++ io/src/main/java/class101/back/io/PostRepository.java
@@ -1,9 +1,14 @@
 package class101.back.io;
 
 import java.util.List;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+@Repository
+// extends JpaRepository<Post, Long>
+public interface PostRepository extends ElasticsearchRepository<Post, String> {
 
-  List<Post> findAllByContentContains(String content);
+//  List<Post> findAllByContentContains(String content);
+  List<Post> findByContent(String content);
 }
